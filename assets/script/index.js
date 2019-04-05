5f6d522c2d68b8f2813cae12ec63a4d36a4962f2
BNgoum <benjamin.ngoum@gmail.com>
2019-04-05 18:49:56 +0200

    Premier commit avec les pages accueil et projets

---
+++ assets/script/index.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+    $('.cl_projets--item').on('mouseover', function() {
+        let src = $(this).children('.cl_projets--bg').attr('src');
+
+        $('.cl_projets--full-bg').css('background-image', 'url(' + src + ')');
+
+        $(this).children('.cl_projets--titre').addClass('cl_projet--titre-active');
+        $(this).children('.cl_projets--sous-titre').addClass('cl_projet--sous-titre-active');
+        
+        $('.cl_projets--item').children('.cl_projets--bg').css('opacity', 0);
+        $('.cl_projets--titre, .cl_projets--sous-titre').css('opacity', 0.5);
+        $(this).children('.cl_projets--titre, .cl_projets--sous-titre').css('opacity', 1);
+    })
+    
+    $('.cl_projets--item').on('mouseout', function() {
+        $('.cl_projets--full-bg').css('background-image', 'none');
+
+        $('.cl_projets--titre').removeClass('cl_projet--titre-active');
+        $('.cl_projets--sous-titre').removeClass('cl_projet--sous-titre-active');
+
+        $('.cl_projets--item').children().css('opacity', 1);
+    })
+
+    $('#btn-burger-menu, #btn-fermer-menu').on('click', function(){
+        $('#menu-content').toggleClass('isOpen');
+    })
+});
