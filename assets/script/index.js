a13804dc587edba90f9d9abe95dd677642b1003d
BNgoum <benjamin.ngoum@gmail.com>
2019-04-12 11:58:03 +0200

    Ajout des liens vers les autres écrans.

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,15 +1,15 @@
 $( document ).ready(function() {
     $('.cl_projets--item').on('mouseover', function() {
-        let src = $(this).children('.cl_projets--bg').attr('src');
+        let src = $(this).find('.cl_projets--bg').attr('src');
 
         $('.cl_projets--full-bg').css('background-image', 'url(' + src + ')');
 
-        $(this).children('.cl_projets--titre').addClass('cl_projet--titre-active');
-        $(this).children('.cl_projets--sous-titre').addClass('cl_projet--sous-titre-active');
+        $(this).find('.cl_projets--titre').addClass('cl_projet--titre-active');
+        $(this).find('.cl_projets--sous-titre').addClass('cl_projet--sous-titre-active');
         
-        $('.cl_projets--item').children('.cl_projets--bg').css('opacity', 0);
+        $('.cl_projets--item').find('.cl_projets--bg').css('opacity', 0);
         $('.cl_projets--titre, .cl_projets--sous-titre').css('opacity', 0.5);
-        $(this).children('.cl_projets--titre, .cl_projets--sous-titre').css('opacity', 1);
+        $(this).find('.cl_projets--titre, .cl_projets--sous-titre').css('opacity', 1);
     })
     
     $('.cl_projets--item').on('mouseout', function() {
@@ -18,7 +18,7 @@ $( document ).ready(function() {
         $('.cl_projets--titre').removeClass('cl_projet--titre-active');
         $('.cl_projets--sous-titre').removeClass('cl_projet--sous-titre-active');
 
-        $('.cl_projets--item').children().css('opacity', 1);
+        $('.cl_projets--item').find('.cl_projets--bg').css('opacity', 1);
     })
 
     $('#btn-burger-menu, #btn-fermer-menu').on('click', function(){
